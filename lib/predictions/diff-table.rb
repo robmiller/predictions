85eea195b02fc60e7887e6ca4b8661d086787c31
Rob Miller <rob@bigfish.co.uk>
2017-08-07 16:30:44 +0100

    Fuzzy matching for team names
    Nobody's actually going to type team names exactly how they appear in
    the API; this change means that, if it doesn't get an exact match, it'll
    intelligently guess.
    
    First it will look for any team names that have an edit distance of less
    than 5 from the given name – so that means 5 insertions, deletions, or
    substitutions. That will catch simple typos, the omission of "FC" at the
    end of a name, or something like that.
    
    Then it will use White similarity, which looks at adjacent letter pairs
    common to both strings. It will pick the most similar match of any that
    score over 0.5.
    
    If it still doesn't have a match that point, it will give up.
    
    Further reading on White Similarity:
    http://www.catalysoft.com/articles/StrikeAMatch.html

--- lib/predictions/diff-table.rb
+++ lib/predictions/diff-table.rb
@@ -1,3 +1,5 @@
+require "text"
+
 module Predictions
   class DiffTable
     def initialize(actual_table)
@@ -7,6 +9,31 @@ module Predictions
     def diff(other_table)
       other_table.map do |guessed|
         real = actual_table.find { |p| p[:team] == guessed[:team] }
+
+        # No exact match for this team. Are there any teams that have an
+        # edit distance of <= 5 (i.e. missing an "FC", or something
+        # simple like that)?
+        unless real
+          real = actual_table.find { |p| Text::Levenshtein.distance(p[:team], guessed[:team]) <= 5 }
+        end
+
+        # Still no match. Let's try White similarity
+        unless real
+          @white ||= Text::WhiteSimilarity.new
+          real =
+            actual_table
+              .select  { |p| @white.similarity(p[:team], guessed[:team]) >= 0.5 }
+              .sort_by { |p| @white.similarity(p[:team], guessed[:team]) }
+              .last
+        end
+
+        unless real
+          raise "No team name matched: I was given #{guessed[:team]}"
+        end
+
+        # Normalise team names
+        guessed[:team] = real[:team]
+
         guessed[:difference] = (real[:position] - guessed[:position]).abs
         guessed[:points_difference] = (real[:points] - guessed[:points]).abs
         guessed
